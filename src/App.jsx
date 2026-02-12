33038438463461b37e7a9f43f830675477a996ae
Salvo-Raia <salvo.raia@outlook.it>
2026-02-12 17:44:40 +0100

    Add movie genre selection input

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,28 @@ import './App.css';
 import movieList from './data/movielist';
 
 export default function App() {
-  const movieGenres = movieList.map((singleMovie) => singleMovie.genre);
-  return console.log(movieGenres);
+    const movieGenres = movieList.map((singleMovie, index) => {
+      return <option key={index} value={singleMovie.genre}>
+      {singleMovie.genre}
+      </option>
+    })
+
+
+ 
+  return <div className='container'>
+            <header>
+             <h1>Movie list</h1>
+             <p>Seleziona un genere per visionare i film disponibili</p>
+            </header>
+            <main>
+              <div className="input-group mb-3">
+                <select className="form-select" id="inputGroupSelect02">
+                  {movieGenres}
+                </select>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Genere</label>
+              </div>
+            </main>
+         </div>
 }
 
 
